e31fd821aa4ba79e693def9bd0640026f34cae80
mike <mikeramirez-14-31@hotmail.com>
2019-05-18 22:39:44 -0600

    modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+  import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { PersonaService } from './service/persona.service';
 import { NotificationService } from './service/notification.service';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -23,6 +23,21 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
     console.log('on click');
   }
 
+
+  public doNotificationSubscription(): void{
+
+    try{
+      this.notificationService.getPersonaNotification().subscribe((result) =>{
+        this.handleMessageReceived(result);
+      });
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  private handleMessageReceived(message: any): void {
+    console.log('Menseje recibido' + JSON.stringify(message));
+  }
 
   public enviarFormulario(): void {
     console.log('Datos de formulario:' + JSON.stringify(this.formGroup.value));
@@ -117,6 +132,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
     console.log('on init');
 
+    this.doNotificationSubscription()
     // iniciar formulario
     this.initForm();
 
